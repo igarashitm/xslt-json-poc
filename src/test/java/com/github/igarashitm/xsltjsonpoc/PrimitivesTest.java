3c3017dc5838283a830c1e5ff9871078fe5855b4
Tomohisa Igarashi <tm.igarashi@gmail.com>
2025-03-28 12:33:37 -0400

    fix primitive, JSON string primitive requires double quotations

--- src/test/java/com/github/igarashitm/xsltjsonpoc/PrimitivesTest.java
+++ src/test/java/com/github/igarashitm/xsltjsonpoc/PrimitivesTest.java
@@ -2,15 +2,18 @@ package com.github.igarashitm.xsltjsonpoc;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PrimitivesTest extends TestBase {
     @Test
-    public void testPrimitive() throws Exception {
+    public void testTopmostPrimitive() throws Exception {
         loadYamlRoute( "03-primitives/primitives.yaml");
         var template = context.createFluentProducerTemplate();
-        // XSLT json-to-xml() doesn't recognize primitives as JSON and throw an exception
-        var exchange = template.withBody("hello").to("direct:mapping").send();
-        assertNotNull(exchange.getException());
+
+        var exchange = template.withBody("\"hello\"").to("direct:mapping").send();
+        assertEquals("\"hello\"", exchange.getMessage().getBody());
+
+        exchange = template.withBody("123456").to("direct:mapping").send();
+        assertEquals("123456", exchange.getMessage().getBody());
     }
 }
